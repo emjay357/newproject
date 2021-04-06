3021aa97ac7e273a51bcf07a98bd5e9542a13d0d
emjay357 <harynguyen357@gmail.com>
2021-04-06 16:51:39 +0700

    update

--- product.php
+++ product.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Modal</title>   
+    <title>Document</title>   
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>    
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">  
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
-    <link rel="stylesheet" href="./responsive.css">
     <link rel="stylesheet" href="./styleneed.css">
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./product.css">
-    
-    
-    <link rel="stylesheet" href="./clickbox.css">
-    <script defer src="./clickbox.js"></script>
-
 </head>
-<body>
-    <div style="background-color: black;">
+<body>  
+<?php
+    require "database.php";
+    if(isset($_GET['productid']) == TRUE){
+    $productid = $_GET['productid'];
+    $product = new product;   
+    $data = $product->productsearch($productid);
+    $productname = $data["productname"];
+    $productimg = $data["productimg"];
+}
+?>
+   <div style="background-color: black;">
         <div class="container">
             <div class="row">
                 <div class="col-sm-3">
                     <div>
-                        <h1><a href="./index.html">
+                        <h1><a href="./index.php">
                             <img src="./image/logofix6.png" style="height: 65px; ">
                         </a></h1>
                     </div>
@@ -41,11 +41,11 @@
                     <div class="row border">
                         <div class="search-box">
                             <div>
-                                <form id="form-search" role="form" method="GET" action="/search">
+                                <form id="form-search" role="form" method="GET" action="">
                                     <div>
                                         <div class="input-group ml-4" style="width: 100%">
                                             <input type="text" name="search" id="search" class="form-control"
-                                                   placeholder="Bạn cần tìm gì...">
+                                                   placeholder="findwhat">
                                             <span class="input-group-btn">
                                                 <button id="btn-search" value="search" class="btn btn" style="background-color: rgb(254, 209, 1);">
                                                     <i class="fa fa-search"></i>
@@ -66,7 +66,7 @@
             <div class="row">
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        <li><a class="taskbar" href="index.html">Home</a></li>
+                        <li><a class="taskbar" href="index.php">Home</a></li>
                         <li><a class="taskbar" href="">Introduction</a></li>
                         <li><a class="taskbar" href="#">Our Product</a></li>
                         <li><a class="taskbar" href="#">Cart</a></li>
@@ -78,8 +78,8 @@
             </div>
         </div>
     </div>
-    <div style="background-color: black;" class="carousel-inner" role="listbox">
-        <div id="backgroundimg">
+    <div style="background-color: black;" class="carousel-inner overwirte" role="listbox" id="backgroundimgproduct">
+        <div>
             <div class="single-slide">
                 <div class="slide-bg slide-one"></div>
                 <div class="slide-text-wrapper">
@@ -87,11 +87,12 @@
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-6 col-md-offset-6">
+
                                     <div class="slide-content">
                                         <h2>We are awesome</h2>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi ab molestiae minus reiciendis! Pariatur ab rerum, sapiente ex nostrum laudantium.</p>
-                                        <a href="" class="readmore">Learn more</a>
+                                        <a class="readmore">Learn more</a>
                                     </div>
                                 </div>
                             </div>
@@ -101,34 +102,5 @@
             </div>
         </div>
     </div>
-    <p class="tittle" style="margin-left: 565px; font-family: Garamond; margin-top: 15px;">TOP SELLING</p>
-    <div class="productdiv">
-        <div>
-            <img src="./image/image/topselling1.jpg" class="productimg">
-            <img src="./image/image/topselling5.jpg" class="productimg">
-            <img src="./image/image/topselling3.jpg" class="productimg">
-            <img src="./image/image/topselling4.jpg" class="productimg">
-            <img src="./image/image/topselling2.jpg" class="productimg">
-
-        </div>
-        <div>
-            <a class="productname" id="productname1">LG Smart Inverter</a>
-            <a class="productname" id="productname2">Mini Mocato Air M503</a>
-            <a class="productname" id="productname3">T24034WHT Microwave</a>
-            <a class="productname" id="productname4">Panasonic Countertop</a>
-            <a class="productname" id="productname5">Toshiba Inverter RAS-H</a>
-        </div>
-    </div>
-    <button data-modal-target="#modal">Open Modal</button>
-  <div class="modal" id="modal">
-    <div class="modal-header">
-      <div class="title">Example Modal</div>
-      <button data-close-button class="close-button">&times;</button>
-    </div>
-    <div class="modal-body">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod alias ut illo doloremque eum ipsum obcaecati distinctio debitis reiciendis quae quia soluta totam doloribus quos nesciunt necessitatibus, consectetur quisquam accusamus ex, dolorum, dicta vel? Nostrum voluptatem totam, molestiae rem at ad autem dolor ex aperiam. Amet assumenda eos architecto, dolor placeat deserunt voluptatibus tenetur sint officiis perferendis atque! Voluptatem maxime eius eum dolorem dolor exercitationem quis iusto totam! Repudiandae nobis nesciunt sequi iure! Eligendi, eius libero. Ex, repellat sapiente!
-    </div>
-  </div>
-  <div id="overlay"></div>
 </body>
 </html>
